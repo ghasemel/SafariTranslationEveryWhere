5a8b89283fc68841d87cc6afee21eba23e58c414
Ghasem <ghasem.elyasi@gmail.com>
2024-11-24 21:05:39 +0100

    initial commit

--- SafariTranslationEveryWhere Extension/Resources/background.js
+++ SafariTranslationEveryWhere Extension/Resources/background.js
@@ -1,6 +1,18 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+// Create the context menu item
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "translate-page",
+        title: "Translate Page",
+        contexts: ["all"] // Adds this option to all context menus
+    });
+    console.log("Context menu item created!");
+});
 
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "translate-page") {
+        const translateUrl = `https://translate.google.com/translate?sl=auto&tl=en&u=${encodeURIComponent(
+            tab.url
+        )}`;
+        chrome.tabs.update(tab.id, { url: translateUrl });
+    }
 });
--- SafariTranslationEveryWhere Extension/Resources/content.js
+++ SafariTranslationEveryWhere Extension/Resources/content.js
@@ -1,7 +1,6 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+// content.js
+document.addEventListener("contextmenu", (event) => {
+    safari.extension.dispatchMessage("showTranslateOption", {
+        pageUrl: window.location.href,
+    });
 });
